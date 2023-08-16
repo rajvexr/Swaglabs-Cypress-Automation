47a5f2148b691154f6edb6b4c7da0ffffb7dbba8
Rajveer <thers3001@gmail.com>
2023-08-16 17:18:58 +0100

    Adding cucumber setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,27 @@
 const cypress = require("cypress");
 const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", browserify.default(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
 
+  e2e: {
+    setupNodeEvents,
     specPattern: 'cypress/integration/e2e/*js',
     screenshotsFolder: 'cypress/failure/screenshots',
     baseUrl: 'https://www.saucedemo.com/',
-    
-    
   },
+
 });
